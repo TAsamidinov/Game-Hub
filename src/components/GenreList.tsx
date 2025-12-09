2289889df98b2e61d1d962cd87a3f699095667ad
Temirlan <temirlan.asamidinov@gmail.com>
2025-12-09 11:13:36 -0600

    Highlight the Selected Genre

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -5,9 +5,10 @@ import { Spinner } from "@chakra-ui/react"
 
 interface Props {
   onSelectGenre: (genre: Genre) => void;
+  selectedGenre: Genre | null;
 }
 
-const GenreList = ({onSelectGenre}: Props) => {
+const GenreList = ({selectedGenre, onSelectGenre}: Props) => {
   const { data, isLoading, error } = useGenres();
 
   if (error) return null;
@@ -23,7 +24,7 @@ const GenreList = ({onSelectGenre}: Props) => {
                 borderRadius={8}
                 src={getCroppedImageUrl(genre.image_background)}
                 />
-                <Button onClick={() => onSelectGenre(genre)} variant={'link'}  fontSize={'lg'}> {genre.name} </Button>
+                <Button fontWeight={genre.id === selectedGenre?.id ? 'bold' : 'normal'} onClick={() => onSelectGenre(genre)} variant={'link'}  fontSize={'lg'}> {genre.name} </Button>
             </HStack>
             </ListItem>
         ))}
